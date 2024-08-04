5862d51fb9394c25e94ff993bb5b9b1a6271380f
lxnd <lxnd@protonmail.com>
2024-08-04 13:12:17 +0200

    Update site

---
+++ _pelican/content/pages/home.md
@@ -0,0 +1,15 @@
+---
+Title: Home
+---
+
+Hello ! 
+
+Bienvenu sur mon site personnel.
+
+# Moi en 5 secondes
+
+Je m'appelle Alexandre.
+
+# Moi en 5 minutes
+
+Voir ma page [à propos](/pages/a-propos-de-moi.html).
--- _pelican/pelicanconf.py
+++ _pelican/pelicanconf.py
@@ -1,3 +1,6 @@
+from pelican.readers import MarkdownReader
+from pelican.settings import DEFAULT_CONFIG
+
 AUTHOR = 'Alexandre Petit'
 SITENAME = 'Alexandre Petit'
 SITEURL = ""
@@ -9,6 +12,8 @@ TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = 'fr'
 
+HOME, _ = MarkdownReader(DEFAULT_CONFIG.copy()).read("content/pages/home.md")
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -16,25 +21,7 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (
-    ("Pelican", "https://getpelican.com/"),
-    ("Python.org", "https://www.python.org/"),
-    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-    ("You can modify those links in your config file", "#"),
-)
-
-# Social widget
-SOCIAL = (
-    ("You can add links in your config file", "#"),
-    ("Another social link", "#"),
-)
-
-DEFAULT_PAGINATION = False
-
-# Uncomment following line if you want document-relative URLs when developing
-# RELATIVE_URLS = True
-
+# Désactive la génération des pages suivantes
 TAG_SAVE_AS = ''
 TAGS_SAVE_AS = ''
 CATEGORY_SAVE_AS = ''
